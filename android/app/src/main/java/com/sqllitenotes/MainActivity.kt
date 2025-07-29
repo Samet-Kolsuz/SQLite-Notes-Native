86ccab7d8572741d1f0efe74717cef901e84d416
Samet-Kolsuz <sametklsz00@gmail.com>
2025-07-30 00:32:32 +0300

    Initial commit
    Generated by react-native@0.80.2

---
+++ android/app/src/main/java/com/sqllitenotes/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sqllitenotes
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "sqllitenotes"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
